9a54a20413ca121e3f884709aedd5be21c7cca7f
Connor Webdale <CWebdale006@west-mec.org>
2019-03-08 15:28:02 -0700

    Event Handlers + Event Listeners
    Wrote several functions and for loops to affect all cells when an event is initiated. Used to change background color of cells

--- jpf_hanjie.js
+++ jpf_hanjie.js
@@ -58,6 +58,10 @@
 
 window.onload = init();
 
+var puzzleCells;
+
+var cellBackground;
+
 function init() {
       // Inserts the title for the first puzzle 
       document.getElementById("puzzleTitle").innerHTML = "Puzzle 1";
@@ -72,7 +76,11 @@ function init() {
       for (let i = 0; i < puzzleButtons.length; i++) {
             puzzleButtons[i].onclick = swapPuzzle;
       }
+      setupPuzzle();
 }
+
+// Adds an event listener for the mouseup event 
+document.addEventListener("mouseup", endBackground);
 
 function swapPuzzle(e) {
       // Retrieve the ID of the clicked button 
@@ -89,14 +97,49 @@ function swapPuzzle(e) {
                   document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle1Hint, puzzle1Rating, puzzle1);
                   break;
             case "puzzle2":
-                  document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle2Hint, puzzle2Rating, puzzle2)
+                  document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle2Hint, puzzle2Rating, puzzle2);
                   break;
-            default:
+            case "puzzle3":
+                  document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle3Hint, puzzle3Rating, puzzle3);
+      }
 
-                  break;
+      setupPuzzle();
+}
+
+function setupPuzzle() {
+      // Match all of the data cells in the puzzle 
+      puzzleCells = document.querySelectorAll("table#hanjieGrid td");
+
+      // Set the initial color of each cell to gold 
+      for (let i = 0; i < puzzleCells.length; i++) {
+            puzzleCells[i].style.backgroundColor = "rgb(233, 207, 29)";
+
+            // Set the cell background color in reposonse to the mousedown event 
+            puzzleCells[i].onmousedown = setBackground;
       }
 }
 
+function setBackground(e) {
+      cellBackground = "rgb(101, 101, 101)";
+
+      e.target.style.backgroundColor = cellBackground;
+
+      // Create an event listener for every puzzle cell 
+      for (var i = 0; i < puzzleCells.length; i++) {
+            puzzleCells[i].addEventListener("mouseenter", extendBackground);
+      }
+}
+
+function extendBackground(e) {
+      e.target.style.backgroundColor = cellBackground;
+}
+
+function endBackground() {
+      // Removes the event listener for every puzzle cell 
+      for (let i = 0; i < puzzleCells.length; i++) {
+            puzzleCells[i].removeEventListener("mouseenter", extendBackground);
+      }
+}
 
 
 
